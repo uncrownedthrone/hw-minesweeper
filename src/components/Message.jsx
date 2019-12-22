5feefd4dc0bba3c5d7e1eb5c1e111e0b72b3f142
Justin Swiger <justin.swiger@me.com>
2019-12-22 09:38:47 -0500

    deployed

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Message = props => {
-  if (props.displayResult === 'new') {
+  if (props.displayresult === ' ') {
+    return <h2 className='wlmessage'>Choose A Difficulty</h2>
+  } else if (props.displayResult === 'new') {
     return <h2 className='wlmessage'>Choose A Difficulty</h2>
   } else if (props.displayResult === 'playing') {
     return <h2 className='wlmessage'>You're Playing!</h2>
